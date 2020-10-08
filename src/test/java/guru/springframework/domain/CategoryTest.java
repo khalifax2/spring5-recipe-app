50c345ffa5dc1c046db8f641061d9d9052a9566f
jericho <echocastro18@gmail.com>
2020-10-08 13:48:28 +0800

    Added Maven Failsafe plugin

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
